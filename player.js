12acd6a9568ddba58b20e4eca68eb38f1c8237da
Shayan Golafshani <shayangolafsh@gmail.com>
2021-04-23 08:40:13 -0600

    Started outline of player.js

---
+++ player.js
@@ -0,0 +1,23 @@
+class Player{
+
+    //one instance is a computer the other is a player maybe we can base
+    //if off a boolean value that's passed into the constructor
+
+    constructor(){
+        //key-value pairs
+
+
+
+    }
+
+    //methods
+
+
+
+
+
+
+
+}
+
+module.exports = Player
