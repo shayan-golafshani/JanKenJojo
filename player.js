e37c95d11a54931392f56b91364d9b23f31dfddf
Shayan Golafshani <shayangolafsh@gmail.com>
2021-04-23 12:39:53 -0600

    Add game.js skeleton and the assets for the second version of the game

--- player.js
+++ player.js
@@ -3,13 +3,21 @@ class Player{
     //one instance is a computer the other is a player maybe we can base
     //if off a boolean value that's passed into the constructor
 
-    constructor(){
+    constructor(userObj){
         //key-value pairs
+        this.name = userObj.name || 'Default';
         this.wins = 0;
         this.handUser = 0;
         this.handComputer = 0;
 
     }
+
+    /*
+    constructor - properties should include: name (ex: 'hannah'), token (ex: '⭐️'), wins (ex: 0)
+    saveWinsToStorage
+    retrieveWinsFromStorage
+    takeTurn
+    */
 
     //methods
     throwHandUser(userClickedImg){
@@ -25,7 +33,7 @@ class Player{
     }
 
     playRockPaperScissorsSpock(){
-        
+
     }
 
 /* 
