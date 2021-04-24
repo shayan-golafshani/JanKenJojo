05f69d901140b477b0711c823233cc1e73167e78
Shayan Golafshani <shayangolafsh@gmail.com>
2021-04-24 13:46:14 -0600

    Rename function

--- game.js
+++ game.js
@@ -2,7 +2,6 @@
 // Two Player instances
 // A way to keep track of the data for the game board
 // A way to keep track of the selected game type
-// A way to keep track of which player’s turn it currently is
 // A way to check the Game’s board data for win conditions
 // A way to detect when a game is a draw (no one has won)
 // A way to reset the Game’s board to begin a new game
@@ -27,7 +26,7 @@ class Game {
     }
 
 
-    compareFighters(){
+    findWinner(){
         var userMove = this.player.takeTurn();
         var computerMove = this.computerEnemy.takeTurn();
 
@@ -35,7 +34,12 @@ class Game {
         //if()
     }
 
+    changeGame(){
+
+    }
+
     resetGame(){
+        setTimeout()
         this.retrieveWins();
     }
 
