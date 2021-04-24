3981dfb8d833538a514646f39ff7f1480326380c
Shayan Golafshani <shayangolafsh@gmail.com>
2021-04-24 11:25:34 -0600

    Add IDs and querySelectors to all the fighters.

--- main.js
+++ main.js
@@ -2,6 +2,17 @@
 var changeGameBtn = document.querySelector('#changeGameBtn');
 var classicGame = document.querySelector('.game-one');
 var jojoGame = document.querySelector('.game-two');
+
+//fighter query selectors
+var rockFighter = document.querySelector('#rockFighter');
+var paperFighter = document.querySelector('#paperFighter"');
+var scissorsFighter = document.querySelector('#scissorsFighter');
+
+var dioFighter = document.querySelector('#dioFighter');
+var josukeFighter = document.querySelector('#josukeFighter');
+var jotaroFighter = document.querySelector('#jotaroFighter');
+var kiraFighter = document.querySelector('#kiraFighter');
+var koichiFighter = document.querySelector('#koichiFighter');
 //add query selectors for all your sets of fighters!
 
 //Event listeners
