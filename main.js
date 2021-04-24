09334badf5e74da6cb389e5fb686c331a269d6a2
Shayan Golafshani <shayangolafsh@gmail.com>
2021-04-23 21:51:51 -0600

    Add query selectors and script tags to link JS and HTML

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 //querySelectors
 var changeGameBtn = document.querySelector('.changeGameBtn');
+var classicGame = document.querySelector('.game-one');
+var jojoGane = document.querySelector('.game-two')
 
 //Event listeners
-changeGameBtn.addEventListener('click', someFunction);
+changeGameBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    someFunction();
+});
+classicGame.addEventListener('click', someFunction);
+jojoGane.addEventListener('click', someFunction);
 
 function someFunction(){
-    
+console.log('Im working!!!!')
 }
