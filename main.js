e8cc1304ef6ec794ff22a513a8e169b91f03ea65
Shayan Golafshani <shayangolafsh@gmail.com>
2021-04-23 20:07:19 -0600

    Start buildout of game and player

---
+++ main.js
@@ -0,0 +1,9 @@
+//querySelectors
+var changeGameBtn = document.querySelector('.changeGameBtn');
+
+//Event listeners
+changeGameBtn.addEventListener('click', someFunction);
+
+function someFunction(){
+    
+}
--- player.js
+++ player.js
@@ -3,9 +3,9 @@ class Player{
     //one instance is a computer the other is a player maybe we can base
     //if off a boolean value that's passed into the constructor
 
-    constructor(userObj){
+    constructor(name){
         //key-value pairs
-        this.name = userObj.name || 'Default';
+        this.name = name || 'Fighter';
         this.wins = 0;
         this.handUser = 0;
         this.handComputer = 0;
